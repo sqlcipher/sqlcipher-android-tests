752895448224a78c2baa22bfe85181c36b7cee3a
Nick Parker <nparker@zetetic.net>
2014-04-23 11:43:46 -0500

    Use latest SQLCipher for Android 3.1.0 binaries

--- src/main/java/net/zetetic/tests/PragmaCipherVersionTest.java
+++ src/main/java/net/zetetic/tests/PragmaCipherVersionTest.java
@@ -5,7 +5,7 @@ import net.sqlcipher.database.SQLiteDatabase;
 
 public class PragmaCipherVersionTest extends SQLCipherTest {
 
-    private final String CURRENT_CIPHER_VERSION = "3.0.1";
+    private final String CURRENT_CIPHER_VERSION = "3.1.0";
 
     @Override
     public boolean execute(SQLiteDatabase database) {
