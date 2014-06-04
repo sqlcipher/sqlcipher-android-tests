43a257f30e1a20aebd26191e4059bf4b30bf3ab0
Nick Parker <nparker@zetetic.net>
2014-06-04 16:54:20 -0500

    Add test for nested transactions

--- src/main/java/net/zetetic/QueryHelper.java
+++ src/main/java/net/zetetic/QueryHelper.java
@@ -16,4 +16,15 @@ public class QueryHelper {
         }
         return value;
     }
+
+    public static int singleIntegerValueFromQuery(SQLiteDatabase database, String query){
+        Cursor cursor = database.rawQuery(query, new String[]{});
+        int value = 0;
+        if(cursor != null){
+            cursor.moveToFirst();
+            value = cursor.getInt(0);
+            cursor.close();
+        }
+        return value;
+    }
 }
