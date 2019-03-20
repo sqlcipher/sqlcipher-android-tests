6996c4208d23810f647cd00c530d5496acf9cbd9
Nick Parker <nparker@zetetic.net>
2019-03-20 13:36:26 -0500

    Include local community build of SQLCipher for Android 4.1.2

--- app/src/main/java/net/zetetic/tests/JavaClientLibraryVersionTest.java
+++ app/src/main/java/net/zetetic/tests/JavaClientLibraryVersionTest.java
@@ -4,7 +4,7 @@ import net.sqlcipher.database.SQLiteDatabase;
 
 public class JavaClientLibraryVersionTest extends SQLCipherTest {
 
-  String expectedClientLibraryVersion = "4.1.1";
+  String expectedClientLibraryVersion = "4.1.2";
 
   @Override
   public boolean execute(SQLiteDatabase database) {
